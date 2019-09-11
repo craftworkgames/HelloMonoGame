961898f408c2abf25399b37989f5b44b51b2518e
Dylan Wilson <dylan@craftworkgames.com>
2019-09-11 22:14:21 +1000

    initial commit

---
+++ HelloMonoGame/Game1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace HelloMonoGame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphicsDeviceManager;
+        private SpriteBatch _spriteBatch;
+        private SpriteFont _spriteFont;
+
+        public Game1()
+        {
+            _graphicsDeviceManager = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _spriteFont = Content.Load<SpriteFont>("Arial");
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            var keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(_spriteFont, "Hello MonoGame!", new Vector2(100, 100), Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# HelloMonoGame
-Hello World with MonoGame on .NET Framework 4.7.2
+# Hello MonoGame
+
+Hello World with MonoGame.
+
+I created this project because MonoGame doesn't come with project templates for Visual Studio 2019.
+
+If you're using Visual Studio 2019 and you don't have Visual Studio 2017 installed you can clone or download this repository in place of using the project templates.
+
+This project also has a few other tweaks:
+
+ - It targets .NET Framework 4.7.2
+ - Uses the MonoGame.Framework.DesktopGL NuGet package
+
+![screenshot](screenshot.png)
